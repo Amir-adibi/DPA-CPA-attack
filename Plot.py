0020c57711903b26d0006eb228e6658b11e43508
Amir Adibi <amir.adibinia@gmail.com>
2023-01-22 21:08:46 +0330

    initial commit

---
+++ Plot.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from load_data import *
+
+class Plot:
+
+    def __init__(self, traces, plotting=True, number_of_samples=370_000, start_offset=44700, end_offset=330_300):
+        self.traces = traces
+        self.plotting = plotting
+
+        self.number_of_samples = number_of_samples
+        self.start_offset = start_offset
+        self.end_offset = end_offset
+
+    def plot(self):
+        if self.plotting:
+            y1 = np.array(self.traces[0])
+            traces = y1.reshape((1, self.number_of_samples))
+            x1 = np.arange(self.number_of_samples)
+            fig = plt.figure(figsize=(19.2, 10.8))
+            ax1 = fig.add_subplot(4, 1, 1)
+            ax1.plot(x1, y1)
+
+            y2 = y1[self.start_offset:self.start_offset+800]
+            x2 = np.arange(self.start_offset, self.start_offset+800)
+            ax2 = fig.add_subplot(4, 1, 2)
+            ax2.plot(x2, y2)
+
+            y3 = y1[self.end_offset-800:self.end_offset]
+            x3 = np.arange(self.end_offset-800, self.end_offset)
+            ax3 = fig.add_subplot(4, 1, 3)
+            ax3.plot(x3, y3)
+
+            y4 = y1[40_000:90_000]
+            x4 = np.arange(40_000, 90_000)
+            ax4 = fig.add_subplot(4, 1, 4)
+            ax4.plot(x4, y4)
+
+            plt.show()
--- index.py
+++ index.py
@@ -1,6 +1,7 @@
-from AccessData import *
-import numpy as np
 import os
+from AccessData import *
+from Plot import *
+from Attack import *
 
 data_directory = Path("preprocessed_data")
 data = AccessData()
@@ -10,4 +11,12 @@ if not os.listdir(data_directory):  # data_directory is empty
     data.save_plaintext()
 
 traces, plaintexts = data.get_data()
+
+# plotting
+plt = Plot(traces, False)
+plt.plot()
+
+# attacking
+atck = Attack(traces, plaintexts)
+atck.attack()
 
